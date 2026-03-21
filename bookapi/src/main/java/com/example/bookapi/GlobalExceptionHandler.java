e54f500379c1583cf1a6771a101a8ad9a079a5dc
Ahmad Fawad Amiri <ahmadfawad.amiri1@gmail.com>
2026-03-21 02:02:08 +0100

    update GlobalExceptionHandler

--- bookapi/src/main/java/com/example/bookapi/GlobalExceptionHandler.java
+++ bookapi/src/main/java/com/example/bookapi/GlobalExceptionHandler.java
@@ -1,32 +1,36 @@
 package com.example.bookapi;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, Object>> handleValidationException(MethodArgumentNotValidException ex){
-        Map<String, Object> errors = new HashMap<>();
-
-        errors.put("status", HttpStatus.BAD_REQUEST.value());
-        errors.put("errors", ex.getBindingResult()
-                .getFieldErrors().stream()
-                .map(error->error.getDefaultMessage())
-                .collect(Collectors.toList()));
-        errors.put("timestamp", java.time.LocalDateTime.now());
-
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex){
+        List<String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField()  + " : " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(errors, HttpStatus.BAD_REQUEST.value()));
     }
+    public static class ErrorResponse{
+        private List<String> errors;
+        private int status;
 
-
+        public ErrorResponse(List<String> errors, int status){
+            this.errors = errors;
+            this.status= status;
+        }
+        public List<String> getErrors(){
+            return errors;
+        }
+        public int getStatus(){
+            return status;
+        }
+    }
 }
